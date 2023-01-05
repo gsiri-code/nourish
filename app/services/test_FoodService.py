a0c36fc8bc53a41383e82c6af1653aef65718e0a
qqbeach <vapraobpukvr@gmail.com>
2023-01-05 21:50:10 +0200

    Add unittest for FoodService

---
+++ app/services/test_FoodService.py
@@ -0,0 +1,15 @@
+import unittest
+import requests
+from FoodService import *
+
+class TestFoodService(unittest.TestCase):
+
+    def test_search(self):
+        url = f'https://api.nal.usda.gov/fdc/v1/foods/search?query=apple&dataType=&pageSize=25&pageNumber=1&sortBy=dataType.keyword&sortOrder=asc&api_key=1TwA6TX0vWloflfUuqEhWrBhraJzgeWiDVRWfNcZ'
+        test_request = requests.get(url).json()
+        test_food_request = {
+            'foods': test_request['foods']
+        }
+        self.assertEqual(FoodService().search('apple'), test_food_request)
+
+
