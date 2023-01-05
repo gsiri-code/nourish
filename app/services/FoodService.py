07f0cac3b78a76a8d08c69e0860ab62ac016378a
qqbeach <vapraobpukvr@gmail.com>
2023-01-05 21:10:05 +0200

    improved FoodService + made temp test for it

--- app/services/FoodService.py
+++ app/services/FoodService.py
@@ -2,17 +2,17 @@ import requests
 
 
 class FoodService:
-    def __init(self):
+    def __init__(self):
         self.api_key = '1TwA6TX0vWloflfUuqEhWrBhraJzgeWiDVRWfNcZ'
 
     def search(self, query: str, page: int = 1):
         url = f'https://api.nal.usda.gov/fdc/v1/foods/search?query={query}&dataType=&pageSize=25&pageNumber={page}&sortBy=dataType.keyword&sortOrder=asc&api_key={self.api_key}'
-        data = request.get(url).json()
+        data = requests.get(url).json()
         return {
             'foods': data['foods']
         }
 
     def get(self, id: int):
         url = f'https://api.nal.usda.gov/fdc/v1/food/{id}?nutrients=0&api_key={self.api_key}'
-        data = request.get(url).json()
+        data = requests.get(url).json()
         return data
