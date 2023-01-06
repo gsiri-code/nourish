4a5818df86c143becced9eb0bc7b42282f53c893
qqbeach <vapraobpukvr@gmail.com>
2023-01-06 21:44:56 +0200

    pydoc for unittest

--- app/services/TestFoodService.py
+++ app/services/TestFoodService.py
@@ -5,6 +5,7 @@ from FoodService import *
 class TestFoodService(unittest.TestCase):
 
     def test_search(self):
+        "Test search for 'apple' is it equal to FoodService function of search with same search case"
         url = f'https://api.nal.usda.gov/fdc/v1/foods/search?query=apple&dataType=&pageSize=25&pageNumber=1&sortBy=dataType.keyword&sortOrder=asc&api_key=1TwA6TX0vWloflfUuqEhWrBhraJzgeWiDVRWfNcZ'
         test_request = requests.get(url).json()
         test_food_request = {
@@ -12,4 +13,4 @@ class TestFoodService(unittest.TestCase):
         }
         self.assertEqual(FoodService().search('apple'), test_food_request)
 
-
+help(TestFoodService.test_search)
