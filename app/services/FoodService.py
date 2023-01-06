41ae8cfd69c0143a181af4657e3fcaef49c6745a
qqbeach <vapraobpukvr@gmail.com>
2023-01-06 22:04:11 +0200

    add pydoc for FoodService

--- app/services/FoodService.py
+++ app/services/FoodService.py
@@ -3,9 +3,11 @@ import requests
 
 class FoodService:
     def __init__(self):
-        self.api_key = '1TwA6TX0vWloflfUuqEhWrBhraJzgeWiDVRWfNcZ'
+        self.api_key = '1TwA6TX0vWloflfUuqEhWrBhraJzgeWiDVRWfNcZ' # TODO: .env
 
     def search(self, query: str, page: int = 1):
+        "returns information about searched food"
+
         url = f'https://api.nal.usda.gov/fdc/v1/foods/search?query={query}&dataType=&pageSize=25&pageNumber={page}&sortBy=dataType.keyword&sortOrder=asc&api_key={self.api_key}'
         data = requests.get(url).json()
         return {
--- app/services/TestFoodService.py
+++ app/services/TestFoodService.py
@@ -6,6 +6,7 @@ class TestFoodService(unittest.TestCase):
 
     def test_search(self):
         "Test search for 'apple' is it equal to FoodService function of search with same search case"
+
         url = f'https://api.nal.usda.gov/fdc/v1/foods/search?query=apple&dataType=&pageSize=25&pageNumber=1&sortBy=dataType.keyword&sortOrder=asc&api_key=1TwA6TX0vWloflfUuqEhWrBhraJzgeWiDVRWfNcZ'
         test_request = requests.get(url).json()
         test_food_request = {
@@ -13,4 +14,3 @@ class TestFoodService(unittest.TestCase):
         }
         self.assertEqual(FoodService().search('apple'), test_food_request)
 
-help(TestFoodService.test_search)
