a7b7d5bd1c1be4bfa729133b4076189097f3313e
qqbeach <vapraobpukvr@gmail.com>
2023-01-06 23:03:38 +0200

    add return info in pydoc

--- app/services/FoodService.py
+++ app/services/FoodService.py
@@ -3,10 +3,41 @@ import requests
 
 class FoodService:
     def __init__(self):
-        self.api_key = '1TwA6TX0vWloflfUuqEhWrBhraJzgeWiDVRWfNcZ' # TODO: .env
+        self.api_key = '1TwA6TX0vWloflfUuqEhWrBhraJzgeWiDVRWfNcZ'  # TODO: .env
 
     def search(self, query: str, page: int = 1):
-        "returns information about searched food"
+        """returns
+
+        {
+            foods: Array<{
+                 "fdcId": int
+                "description": str
+                "lowercaseDescription": str
+                "dataType": str
+                "gtinUpc": str
+                "publishedDate": str
+                "brandOwner": str,
+                "ingredients": str,
+                "marketCountry": str,
+                "foodCategory": str,
+                "modifiedDate": str,
+                "dataSource": str,
+                "servingSizeUnit": str,
+                "servingSize": int,
+                "householdServingFullText": str,
+                "tradeChannels": str[],
+                "allHighlightFields": str,
+                "score": int,
+                "microbes": [],
+                "foodNutrients": FoodNutrient[],
+                "finalFoodInputFoods": [],
+                "foodMeasures": [],
+                "foodAttributes": [],
+                "foodAttributeTypes": [],
+                "foodVersionIds": []
+             }>
+        }
+        """
 
         url = f'https://api.nal.usda.gov/fdc/v1/foods/search?query={query}&dataType=&pageSize=25&pageNumber={page}&sortBy=dataType.keyword&sortOrder=asc&api_key={self.api_key}'
         data = requests.get(url).json()
