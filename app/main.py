c6bfca5a989ad7853e591200656d071dfcee6ac5
qqbeach <danildzekelev@gmail.com>
2023-01-19 11:53:52 +0200

    Initialize start-page Change double quotes to single to improve readabilty

--- app/main.py
+++ app/main.py
@@ -5,21 +5,26 @@ from services.FoodService import *
 app = Flask(__name__)
 
 
-@app.route("/search/<food_description>")
+@app.route('/')
+def welcome():
+    return render_template('start-page.html')
+
+
+@app.route('/search/<food_description>')
 def search(food_description):
     try:
-        food_list = FoodService().search(food_description)["foods"]
+        food_list = FoodService().search(food_description)['foods']
     except requests.exceptions.Timeout:
-        return "Timeout error, check your internet connection and try again"
+        return 'Timeout error, check your internet connection and try again'
     return render_template('food-search.html', food_list=food_list)
 
 
-@app.route("/food/<food_id>")
+@app.route('/food/<food_id>')
 def food(food_id):
     try:
         food_info = FoodService().get(int(food_id))
     except ValueError:
-        return "Invalid food id"
+        return 'Invalid food id'
 
     print(food_info)
     return render_template('food-details.html', food_info=food_info, nutrients_info=food_info['labelNutrients'])
