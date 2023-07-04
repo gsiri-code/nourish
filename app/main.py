a1363422ea7a7085a1ad5d12a98cffc96a1db29a
qqbeach <danildzekelev@gmail.com>
2023-07-04 18:37:09 +0300

    Change onclick url redirect on welcome-page. Remove useless code in main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, url_for,redirect
+from flask import Flask, request, render_template, url_for, redirect
 from pip._vendor import requests
 
 from services.FoodService import *
@@ -34,36 +34,7 @@ def search():
     except KeyError:
         return f"Your search inquiry doesn't exist, make sure that you haven't typed special symbols"
 
-    return render_template('search_page.html', query=query,food_list=food_list, results_count=food_results_count)
-
-# @app.route('/redirect-kiwi')
-# def redirect_kiwi():
-#     return redirect('http://127.0.0.1:5000/search?q=Kiwi')
-
-@app.route('/redirect-apple')
-def redirect_apple():
-    return redirect('http://127.0.0.1:5000/search?q=Apple')
-@app.route('/redirect-pear')
-def redirect_pear():
-    return redirect('http://127.0.0.1:5000/search?q=Pear')
-@app.route('/redirect-grape')
-def redirect_grape():
-    return redirect('http://127.0.0.1:5000/search?q=Grape')
-@app.route('/redirect-lemon')
-def redirect_lemon():
-    return redirect('http://127.0.0.1:5000/search?q=Lemon')
-@app.route('/redirect-avacado')
-def redirect_avacado():
-    return redirect('http://127.0.0.1:5000/search?q=Avacado')
-@app.route('/redirect-melon')
-def redirect_melon():
-    return redirect('http://127.0.0.1:5000/search?q=Melon')
-@app.route('/redirect-orange')
-def redirect_orange():
-    return redirect('http://127.0.0.1:5000/search?q=Orange')
-@app.route('/redirect-watermelon')
-def redirect_watermelon():
-    return redirect('http://127.0.0.1:5000/search?q=Watermelon')
+    return render_template('search_page.html', query=query, food_list=food_list, results_count=food_results_count)
 
 
 @app.route('/food/<food_id>')
@@ -74,6 +45,7 @@ def food(food_id):
         return 'Invalid food id'
 
     return render_template('food-details.html', food_info=food_info, nutrients_info=food_info['labelNutrients'])
+
 
 @app.route("/search-page")
 def search_page():
